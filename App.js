e00d470328400e43c69ee5d016931a32e01490cd
Ken Wong <canonw@users.noreply.github.com>
2020-10-22 22:47:32 +0000

    Wire Redux

--- App.js
+++ App.js
@@ -1,16 +1,24 @@
 import React from "react";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducer from "./reducers";
+import middleware from "./middleware";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Home from "./components/Home";
+
+const store = createStore(reducer, middleware);
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
